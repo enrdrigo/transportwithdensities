93f0af14811fb192c39da5025ed0b0668188a7b3
enrdrigo <enr.drigo@gmail.com>
2022-11-02 17:50:13 +0100

    fit perfezionamento

--- modules/computespecificheat.py
+++ modules/computespecificheat.py
@@ -64,15 +64,15 @@ def spcecificheat(root, filename, filename_loglammps, nk, posox, UNITS):
     with open(inp['root'] + 'cp.out', 'w') as f:
         for i in range(cpk.shape[0] - 1):
             f.write(
-                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cpk)[i]) + '{}\n'.format(stdcp[i]))
+                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cpk)[i+1]) + '{}\n'.format(stdcp[i+1]))
     with open(inp['root'] + 'cv.out', 'w') as f:
         for i in range(cvk.shape[0] - 1):
             f.write(
-                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cvk)[i]) + '{}\n'.format(stdcv[i]))
+                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cvk)[i+1]) + '{}\n'.format(stdcv[i+1]))
 
     with open(inp['root'] + 'cpenth.out', 'w') as f:
         for i in range(cp_peth.shape[0] - 1):
             f.write(
-                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cp_peth)[i]) + '{}\n'.format(stdcp_peth[i]))
+                '{}\t'.format(G[i + 1] * 10) + '{}\t'.format(np.real(cp_peth)[i+1]) + '{}\n'.format(stdcp_peth[i+1]))
 
     return res
